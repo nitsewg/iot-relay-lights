18cd4e30b2ed44789efd0600da4381aacff92ac1
Weston <mrwastebasket@gmail.com>
2023-11-23 20:07:29 -0600

    adding off

---
+++ timer-off.py
@@ -0,0 +1,16 @@
+# Connect GPIO18 to positive on the IoT relay, connect ground from Pi to ground on relay.
+
+import random
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
+#Set up random sleep time
+randSleep = random.randint(1,1200)
+time.sleep(randSleep)
+
+#Set relay to "on"
+GPIO.output(18,GPIO.LOW)
