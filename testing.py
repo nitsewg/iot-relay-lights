4444678796bdf092f1dea98e70f782563f592481
Weston <mrwastebasket@gmail.com>
2023-11-23 20:06:10 -0600

    initial commit

---
+++ testing.py
@@ -0,0 +1,16 @@
+# Connect GPIO18 to positive on the IoT relay, connect ground from Pi to ground on relay.
+
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
+print("CTRL+C to stop testing")
+
+while 1 == 1:
+    GPIO.output(18,GPIO.HIGH) # Toggle relay on.
+    time.sleep(5)
+    GPIO.output(18,GPIO.LOW) # Toggle relay off.
+    time.sleep(5)
