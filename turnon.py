55aebdff5fbe878532d4caae150748a0b2ae2788
Weston <mrwastebasket@gmail.com>
2023-11-25 13:05:29 -0600

    adding immediate off/on

---
+++ turnon.py
@@ -0,0 +1,12 @@
+# Connect GPIO18 to positive on the IoT relay, connect ground from Pi to ground on relay.
+
+import random
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
+#Set relay to "on"
+GPIO.output(18,GPIO.HIGH)
