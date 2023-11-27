37383e56831a9dff08bee2b0f88f86b2b43531e0
Weston <mrwastebasket@gmail.com>
2023-11-27 10:44:05 -0600

    rolling back again

--- server.py
+++ server.py
@@ -2,24 +2,20 @@ import http.server
 import socketserver
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
 #here you create a new handler, you had a new way to handle get request
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(18,GPIO.OUT)
+        
        #this code execute when a GET request happen, then you have to check if the request happenned because the user pressed the button
         if self.path.find("turnlampon=true") != -1:
             #print("Lamp on")
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setwarnings(False)
-            GPIO.setup(18,GPIO.OUT)
             GPIO.output(18,GPIO.HIGH)
         else:
             #print("Lamp off")
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setwarnings(False)
-            GPIO.setup(18,GPIO.OUT)
             GPIO.output(18,GPIO.LOW)
         return super().do_GET()
 
