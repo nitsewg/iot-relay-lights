dc99b652f84c4511e2b6c4ca75e9a2578d7e5f8c
Weston <mrwastebasket@gmail.com>
2023-11-27 11:06:23 -0600

    no idea what I am doing

--- server.py
+++ server.py
@@ -9,8 +9,7 @@ GPIO.setup(18,GPIO.OUT)
 #here you create a new handler, you had a new way to handle get request
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        lampstate = 0
-       #this code execute when a GET request happen, then you have to check if the request happenned because the user pressed the button
+        #this code execute when a GET request happen, then you have to check if the request happenned because the user pressed the button
         if self.path.find("turnlampon=true") != -1:
             #print("Lamp on")
             GPIO.output(18,GPIO.HIGH)
